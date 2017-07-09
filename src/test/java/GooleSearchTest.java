58e45589ce8b9d7aa1fe7b64ee850c5476f524a6
Denys Ovcharuk <mallakath@gmail.com>
2017-07-09 21:20:56 +0300

    Lesson21 - classwork

--- src/test/java/GooleSearchTest.java
+++ src/test/java/GooleSearchTest.java
@@ -1,10 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.helpers.SubstituteLogger;
+import org.slf4j.impl.Log4jLoggerAdapter;
+import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertTrue;
@@ -16,7 +19,6 @@ import static org.testng.Assert.assertTrue;
 public class GooleSearchTest {
 
     private WebDriver webDriver;
-
     private String googleSearch = "http://www.google.com.ua/";
     private String searchTest = "Selenium";
 
@@ -24,6 +26,7 @@ public class GooleSearchTest {
     public void searchTest(){
         //webDriver = new FirefoxDriver();
         System.setProperty("webdriver.chrome.driver","/media/innos/WebDriver/chromedriver");
+        System.setProperty("log4j.debug", "");
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
@@ -33,10 +36,23 @@ public class GooleSearchTest {
         searchField.sendKeys(searchTest);
         searchField.submit();
         By linkLocator = By.xpath(".//*[@id='rso']/div[2]/div/div[1]/div/div/h3/a");
+        //By linksLocator = By.ByTagName.tagName("a");
+        By linksLocator = By.xpath(".//*[@class='r']");
+        List<WebElement> links = webDriver.findElements(linksLocator);
+
+        assertTrue(!links.isEmpty());
+        System.out.println(links.size());
+        for (WebElement linkE : links) {
+            if(linkE.isDisplayed()&&(linkE.getText().contains("selenium")||linkE.getText().contains("Selenium"))){
+                System.out.println(linkE.getText());
+            }
+        }
+
+
         WebElement link = webDriver.findElement(linkLocator);
         assertTrue(link.getText().contains(searchTest));
         webDriver.quit();
-        webDriver.close();
+        //webDriver.close();
     }
 
 
